bb7fa0f2e63be206c7da35cc35ffef8f2eb69046
Dominik <dmkk1@outlook.com>
2022-04-28 19:40:16 +0200

    Puzzle Platforms init

---
+++ PuzzlePlatforms/Source/PuzzlePlatforms/PuzzlePlatformsGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PuzzlePlatformsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APuzzlePlatformsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APuzzlePlatformsGameMode();
+};
+
+
+
